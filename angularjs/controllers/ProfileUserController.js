da4f5c39074440ff81c93bcc8cedb74c203af539
Dusan <dulerad94@gmail.com>
2017-07-16 02:51:46 +0200

    aaaaa

--- angularjs/controllers/ProfileUserController.js
+++ angularjs/controllers/ProfileUserController.js
@@ -1,12 +1,10 @@
 angular
-  .module("app.controller.profileuser", ['ngMaterial'])
+  .module("app.controller.profileuser", [])
   .controller("ProfileUserController", [
     "$scope",
     "$location",
     "$window",
     function($scope, $location, $window) {
-            $scope.chips=[];
-
 
     }
   ]);
