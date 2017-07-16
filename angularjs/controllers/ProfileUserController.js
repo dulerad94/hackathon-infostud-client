df62aa897c8d9cec3d41adc0fd31150b1196b18a
mstojevski <stojevskimilan@gmail.com>
2017-07-16 02:51:01 +0200

    Profile user added

--- angularjs/controllers/ProfileUserController.js
+++ angularjs/controllers/ProfileUserController.js
@@ -1,12 +1,21 @@
 angular
-  .module("app.controller.profileuser", ['ngMaterial'])
+  .module("app.controller.profileuser", ["ngMaterial"])
   .controller("ProfileUserController", [
     "$scope",
     "$location",
     "$window",
     function($scope, $location, $window) {
-            $scope.chips=[];
+      $scope.chips = [];
+      $scope.states = [{ a: "aaa" }, { a: "bbbbb" }];
 
-
+      $(document).ready(function() {
+        $("select").material_select();
+      });
+      jQuery(function($) {
+        $(document).ready(function() {
+          $("#select").val(newValue);
+          $("#select").material_select();
+        });
+      });
     }
   ]);
